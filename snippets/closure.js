ae48ca1a13423e6f069f5173a4b577e0d8965bcd
Adam Giese <adam.giese@underarmour.com>
2019-08-11 17:45:53 -0500

    Rework partial application example

--- snippets/closure.js
+++ snippets/closure.js
@@ -1,10 +1,11 @@
 import transpose from 'fictional-library';
+// (dir, interval, note) => newNote
 
-const up3M = note => transpose('up', '3M', note);
+const upMajorThird = note => transpose('up', '3M', note);
 
-up3M({note: 'C', octave: 4}); // {note: 'E', octave: 4}
-up3M({note: 'Bb', octave: 2}); // {note: 'D', octave: 3}
+upMajorThird({note: 'C', octave: 4}); // {note: 'E', octave: 4}
+upMajorThird({note: 'Bb', octave: 2}); // {note: 'D', octave: 3}
 
 /* Stage 1 TC39 Native Partial Application Syntax */
 
-const down7m = transpose('down', '7m', ?);
+const downMinorSeventh = transpose('down', '7m', ?);
