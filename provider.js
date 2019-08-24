95a93389f88ba82b102a595d39e4da7c5f460190
Adam Giese <adam.giese@underarmour.com>
2019-08-24 16:39:44 -0500

    Add really really shitty way to get button support for switch

--- provider.js
+++ provider.js
@@ -2,14 +2,27 @@ import React from 'react'
 import useAnimationFrame from './util/useAnimationFrame.js'
 import Unswitch from 'unswitch';
 import fireKeyEvent from './util/fireKeyEvent.js'
-import { updaters, withDeck } from 'mdx-deck'
+import { updaters, withSlide } from 'mdx-deck'
 
 
 export default props => {
+  const slide = React.useContext(withSlide);
   const unswitch = new Unswitch({
     side: 'R',
     buttons: (button, pressed, side) => console.log(`Catch-all - button: ${button} was ${pressed ? 'pressed' : 'released'} on the ${side} side`),
-    b: p => {},
+    b: p => {
+      if (p) {
+        /* i know this shit is ugly but i'm done trying to find a cleaner way */
+        const slideNumber = document.querySelector('#root > div').getAttribute('activeslide')
+        const activeSlide = document.querySelector(`#root > div > div > div > div > div > div:nth-child(${Number(slideNumber) + 1})`)
+        const buttons = Array.from(activeSlide.querySelectorAll('button'));
+        const indexOfFocus = buttons.findIndex(x => document.activeElement === x);
+        const isEnd = buttons.length - 1 === indexOfFocus;
+        if (!isEnd) {
+          buttons[indexOfFocus + 1].focus()
+        }
+      }
+    },
     a: p => {
       if (p) {
         props.update(updaters.next)
@@ -20,11 +33,34 @@ export default props => {
         props.update(updaters.previous)
       }
     },
-    x: p => {},
+    x: p => {
+        /* i know this shit is ugly but i'm done trying to find a cleaner way */
+        const slideNumber = document.querySelector('#root > div').getAttribute('activeslide')
+        const activeSlide = document.querySelector(`#root > div > div > div > div > div > div:nth-child(${Number(slideNumber) + 1})`)
+        const buttons = Array.from(activeSlide.querySelectorAll('button'));
+        const indexOfFocus = buttons.findIndex(x => document.activeElement === x);
+        const isStart = indexOfFocus === 0
+
+        if (!isStart) {
+          buttons[indexOfFocus - 1].focus()
+        }
+    },
     sl: p => {},
     sr: p => {},
     minus: p => {},
-    plus: p => {},
+    plus: p => {
+      if (p) {
+        /* i know this shit is ugly but i'm done trying to find a cleaner way */
+        const slideNumber = document.querySelector('#root > div').getAttribute('activeslide')
+        const activeSlide = document.querySelector(`#root > div > div > div > div > div > div:nth-child(${Number(slideNumber) + 1})`)
+        const buttons = Array.from(activeSlide.querySelectorAll('button'));
+        const hasActiveButtonOnSlide = buttons.some(x => x === document.activeElement)
+
+        if (hasActiveButtonOnSlide) {
+          document.activeElement.click();
+        }
+      }
+    },
     lstick: p => {},
     rstick: p => {},
     home: p => {},
@@ -35,8 +71,10 @@ export default props => {
   });
   useAnimationFrame(() => unswitch.update());
 
+  console.log(props)
+
   return (
-    <div>
+    <div activeSlide={props.index}>
       {props.children}
     </div>
   )
