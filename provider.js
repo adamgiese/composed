dd6bdc50d76cb214031061cc0c9ecd7938d94197
Adam Giese <adam.giese@underarmour.com>
2019-08-24 15:36:56 -0500

    hacky but working switch updater

--- provider.js
+++ provider.js
@@ -2,37 +2,37 @@ import React from 'react'
 import useAnimationFrame from './util/useAnimationFrame.js'
 import Unswitch from 'unswitch';
 import fireKeyEvent from './util/fireKeyEvent.js'
+import { updaters, withDeck } from 'mdx-deck'
 
-const unswitch = new Unswitch({
-  side: 'R',
-  buttons: (button, pressed, side) => console.log(`Catch-all - button: ${button} was ${pressed ? 'pressed' : 'released'} on the ${side} side`),
-  b: p => {},
-  a: p => {
-    if (p) {
-      console.log('should fire right event')
-      fireKeyEvent(document, 39);
-    }
-  },
-  y: p => {
-    if (p) {
-      fireKeyEvent(document, 37);
-    }
-  },
-  x: p => {},
-  sl: p => {},
-  sr: p => {},
-  minus: p => {},
-  plus: p => {},
-  lstick: p => {},
-  rstick: p => {},
-  home: p => {},
-  screenshot: p => {},
-  bumper: p => {}, // L or R
-  trigger: p => {}, // ZL or ZR
-  axes: p => {},
-});
 
 export default props => {
+  const unswitch = new Unswitch({
+    side: 'R',
+    buttons: (button, pressed, side) => console.log(`Catch-all - button: ${button} was ${pressed ? 'pressed' : 'released'} on the ${side} side`),
+    b: p => {},
+    a: p => {
+      if (p) {
+        props.update(updaters.next)
+      }
+    },
+    y: p => {
+      if (p) {
+        props.update(updaters.previous)
+      }
+    },
+    x: p => {},
+    sl: p => {},
+    sr: p => {},
+    minus: p => {},
+    plus: p => {},
+    lstick: p => {},
+    rstick: p => {},
+    home: p => {},
+    screenshot: p => {},
+    bumper: p => {}, // L or R
+    trigger: p => {}, // ZL or ZR
+    axes: p => {},
+  });
   useAnimationFrame(() => unswitch.update());
 
   return (
